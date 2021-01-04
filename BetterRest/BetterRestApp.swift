479f604417fb1f7700fe54d0731bedf275a4f6c2
Katios <kate.chonkina@gmail.com>
2021-01-04 10:31:44 +0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Екатерина Чернова on 04.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
