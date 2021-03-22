c6cd15dcb6f2c618485722e74199674cb804a62f
Katios <kate.chonkina@gmail.com>
2021-03-22 17:02:58 +0600

    word kate

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -24,7 +24,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             
-                Section (header: Text("When you want to wake up?")){
+                Section (header: Text("When you want to wake up? kate")){
 //                    Text("When you want to wake up?")
 //                        .font(.headline)
                     
